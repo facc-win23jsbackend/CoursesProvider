b6731344c66d5598d99ca3ffa6bd526ea7f62ffc
Adam Ottosson <addeecsgo@gmail.com>
2024-05-31 15:04:56 +0200

    virtual added

--- Courses_WebAPI/Entities/ContentEntity.cs
+++ Courses_WebAPI/Entities/ContentEntity.cs
@@ -3,7 +3,7 @@
 public class ContentEntity
 {
     public string? Description { get; set; }
-    public List<string>? Includes { get; set; }
-    public List<string>? Learn {  get; set; }
+    public virtual List<string>? Includes { get; set; }
+    public virtual List<string>? Learn {  get; set; }
     public virtual List<ProgramDetailItemEntity>? ProgramDetails { get; set; }
 }
